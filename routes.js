3803a3eef202560da86b65fbf498328f2404e4a2
riandias1331 <pereirarian@gmail.com>
2025-03-09 16:50:15 -0300

    initial commit

---
+++ routes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const Controller = require("./src/controllers/Controller.js"); 
+const auth = require("./src/middlwares/auth.js"); 
+
+// Rotas privadas (requerem autenticação)
+router.get("/users", auth.authenticateToken, Controller.getUsers); 
+router.delete("/users", auth.authenticateToken, Controller.deletedUserAll); 
+
+
+
+// Rotas públicas
+router.get("/", Controller.renderHome);
+router.get("/Page", Controller.renderPage);
+router.get("/login", Controller.getlogin);
+router.get("/register", Controller.getregister);
+router.post("/register", Controller.register); 
+router.post("/login", Controller.login);
+
+module.exports = router;
